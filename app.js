1100716a83d78db073812f1b0736a3d7b630b547
Christianib003 <91727952+Christianib003@users.noreply.github.com>
2022-04-16 11:29:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const body = document.getElementById('body');
 
 dropDown.style.display = 'none';
 
-icon.addEventListener('click', changeIcon() {
+icon.addEventListener('click', => {
   icon.classList.toggle('fa-times');
   icon.classList.toggle('fa-bars');
 
@@ -15,14 +15,14 @@ icon.addEventListener('click', changeIcon() {
     dropDown.style.display = 'none';
     body.style.overflow = 'visible';
   }
-})
+});
 
-function hideMenu() {
+icon.addEventListener('click', => {
   dropDown.style.display = 'none';
   icon.classList.toggle('fa-times');
   icon.classList.toggle('fa-bars');
   body.style.overflow = 'visible';
-}
+});
 
 const speakersLeft = [{
   class: 'speaker_1',
